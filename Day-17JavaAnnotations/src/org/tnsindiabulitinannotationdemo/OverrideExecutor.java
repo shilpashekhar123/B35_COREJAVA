6e1d7917fc9fcf4f9624771ee344ecadf863f2dd
Shilpa S M <Shilpa S M@LAPTOP-1P02B106>
2022-12-15 22:09:44 +0530

    firstcommit

---
+++ Day-17JavaAnnotations/src/org/tnsindiabulitinannotationdemo/OverrideExecutor.java
@@ -0,0 +1,12 @@
+package org.tnsindiabulitinannotationdemo;
+//driver class
+public class OverrideExecutor {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.display();
+		
+
+	}
+
+}
---
+++ Day-17JavaAnnotations/src/org/tnsindiabulitinannotationdemo/Parent.java
@@ -0,0 +1,14 @@
+package org.tnsindiabulitinannotationdemo;
+//Super class
+public class Parent {
+	
+	public String name="Nita R";
+	public void display()
+	{
+		System.out.println("Parent Name: "+name);
+	}
+	
+	
+	
+
+}
